1d6dba3bb470f079eee4e0ccb16d6f3dab07aa65
alfattahfinance <mhmdaril707@gmail.com>
2026-08-23 15:33:37 +0700

    Fix build: import MediaStore for Excel export

--- UpdatableMainActivity.java
+++ UpdatableMainActivity.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Base64;
 import android.webkit.JavascriptInterface;
 import android.webkit.MimeTypeMap;
